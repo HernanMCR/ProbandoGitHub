5a4a30ac12dc01f0c8980ab1fd386680296bf182
HernanMCR <hernanmcr@kaynlol.com>
2019-09-14 18:42:36 -0500

    Esta es una pequeña modificacion a este proyecto

--- src/git/prueba/pkg1/pkg1/GitPrueba11.java
+++ src/git/prueba/pkg1/pkg1/GitPrueba11.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package git.prueba.pkg1.pkg1;
 
-/**
- *
- * @author herna
- */
 public class GitPrueba11 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public static void main (String [] args)
+    {
+        System.out.println("Hola mundo");
+        
+        System.out.println("Vamos a hacer una funcion Recursion");
+        
+  
     }
     
 }
